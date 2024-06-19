28210a82946d9774d1c16cbdb8afe2c3e4de2d88
KirlYurch <pupyrkin.1970@mail.ru>
2024-06-19 21:30:32 +0500

    hm-5 (фильтрация 2)

--- src/components/FilterItem/FilterItem.tsx
+++ src/components/FilterItem/FilterItem.tsx
@@ -1,6 +1,7 @@
 import styles from "@components/FilterItem/FilterItem.module.css";
 import { useAppDispatch } from "@/hooks";
 import { setFilter } from "@/store/features/playlistSlice";
+import { useState } from "react";
 
 type FilterList = { id: number, name: string };
 type FilterItemProps = {
@@ -9,9 +10,15 @@ type FilterItemProps = {
 
 export default function FilterItem({ FilterList }: FilterItemProps) {
   const dispatch = useAppDispatch();
+  const [selectedAuthors, setSelectedAuthors] = useState<string[]>([]);
 
   function handleItemClick(name: string) {
-    dispatch(setFilter({ author: [name] }));
+    const updatedSelectedAuthors = selectedAuthors.includes(name)
+      ? selectedAuthors.filter(author => author !== name)
+      : [...selectedAuthors, name];
+    
+    setSelectedAuthors(updatedSelectedAuthors);
+    dispatch(setFilter({ author: updatedSelectedAuthors }));
   }
 
   return (
@@ -19,7 +26,7 @@ export default function FilterItem({ FilterList }: FilterItemProps) {
       <ul className={styles.filterList}>
         {FilterList?.map((item) => (
           <li 
-            className={styles.li} 
+            className={`${styles.li} ${selectedAuthors.includes(item.name) ? styles.selected : ""}`} 
             key={item.id}
             onClick={() => handleItemClick(item.name)}
           >
